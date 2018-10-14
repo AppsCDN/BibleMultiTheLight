1d639fa84f2220bc6aa3e82634aa0d7555488522
hotlittlewhitedog <hotlittlewhitedog@gmail.com>
2018-10-14 18:02:31 +0200

    commit1 	modified:   app/src/main/java/org/hlwd/bible/Dal.java 	modified:   app/src/main/java/org/hlwd/bible/DbHelper.java 	modified:   app/src/main/res/values-es/strings.xml 	modified:   app/src/main/res/values-fr/strings.xml 	modified:   app/src/main/res/values-it/strings.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/org/hlwd/bible/DbHelper.java
+++ app/src/main/java/org/hlwd/bible/DbHelper.java
@@ -18,7 +18,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 //<editor-fold defaultstate="collapsed" desc="-- History --">
-// PROD: Bible 3.4,    DbVersion: 23 (?) 2018-10-14
+// PROD: Bible 3.4,    DbVersion: 23 (9) 2018-10-14
 // PROD: Bible 3.3,    DbVersion: 22 (8) 2018-06-10
 // PROD: Bible 3.2,    DbVersion: 21 (8) 2018-05-05
 // PROD: Bible 3.1,    DbVersion: 20 (8) 2018-05-01
@@ -139,6 +139,9 @@ class DbHelper extends SQLiteOpenHelper
                 sql = "DROP INDEX IF EXISTS bible_ndx";
                 _db.execSQL(sql);
 
+                sql = "DROP INDEX IF EXISTS bibleNumber_ndx";
+                _db.execSQL(sql);
+
                 sql = DropTable("bible");
                 _db.execSQL(sql);
 
@@ -151,6 +154,10 @@ class DbHelper extends SQLiteOpenHelper
                 _db.execSQL(sql);
 
                 sql = "CREATE UNIQUE INDEX bible_ndx on bible (id)";
+                _db.execSQL(sql);
+
+                //23
+                sql = "CREATE UNIQUE INDEX bibleNumber_ndx on bible (bbName, bNumber, cNumber, vNumber)";
                 _db.execSQL(sql);
 
                 //Mark: 1=Fav, 2=Bookmark | rating=0..5 | note=personal note | links? //was (never used): sql = "CREATE TABLE bibleNote (bNumber INTEGER, cNumber INTEGER, vNumber INTEGER, changeDt TEXT, mark INTEGER, rating INTEGER, note TEXT, PRIMARY KEY (bNumber, cNumber, vNumber))";
@@ -361,6 +368,14 @@ class DbHelper extends SQLiteOpenHelper
             if (oldVersion < 20)    //1..19 => 20
             {
                 PCommon.SavePref(_context, IProject.APP_PREF_KEY.UI_LAYOUT, "C");
+            }
+            if (oldVersion < 23)    //1..22 => 23
+            {
+                sql = "DROP INDEX IF EXISTS bibleNumber_ndx";
+                _db.execSQL(sql);
+
+                sql = "CREATE UNIQUE INDEX bibleNumber_ndx on bible (bbName, bNumber, cNumber, vNumber)";
+                _db.execSQL(sql);
             }
             //Last
             if (oldVersion < _version)    //1..(last-1) => last
