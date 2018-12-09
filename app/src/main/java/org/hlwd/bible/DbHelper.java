ff214c36b72f567804273e19e397d9e773543e7f
hotlittlewhitedog <hotlittlewhitedog@gmail.com>
2018-12-09 19:20:32 +0100

    commit1 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/org/hlwd/bible/DbHelper.java 	modified:   app/src/main/java/org/hlwd/bible/MainActivity.java 	modified:   app/src/main/java/org/hlwd/bible/PCommon.java 	modified:   app/src/main/res/values-es/strings.xml 	modified:   app/src/main/res/values-fr/strings.xml 	modified:   app/src/main/res/values-it/strings.xml 	modified:   app/src/main/res/values-pt/strings.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/org/hlwd/bible/DbHelper.java
+++ app/src/main/java/org/hlwd/bible/DbHelper.java
@@ -18,7 +18,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 //<editor-fold defaultstate="collapsed" desc="-- History --">
-// PROD: Bible 3.8,    DbVersion: 27 (10)2018-12-04
+// PROD: Bible 3.8,    DbVersion: 27 (10)2018-12-09
 // PROD: Bible 3.7,    DbVersion: 26 (10)2018-11-25
 // PROD: Bible 3.6,    DbVersion: 25 (10)2018-11-18
 // PROD: Bible 3.5,    DbVersion: 24 (10)2018-11-10
